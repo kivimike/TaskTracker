3ae28013a48f5f9c3097905aab8d04ed38659710
Mikhail Kivi <kivimike@gmail.com>
2022-11-11 06:19:30 +0500

    modified card padding, added min and max lines params to taskName textfield, taskname made scrollable

--- lib/components/task_view.dart
+++ lib/components/task_view.dart
@@ -17,8 +17,8 @@ class TaskView extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.transparent,
       body: Padding(
-        padding: const EdgeInsets.only(top: 20, bottom: 100.0, left: 30, right: 30),
-        
+        //padding: const EdgeInsets.only(top: 20, bottom: 100.0, left: 30, right: 30),
+        padding: EdgeInsets.all(16),
         child: Material(
           borderRadius: BorderRadius.circular(30),
           elevation: 1,
@@ -37,13 +37,19 @@ class TaskView extends StatelessWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          SelectableText(taskName,
-                          style: const TextStyle(
-                              color: Colors.black87,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                              letterSpacing: 2),
+                          Expanded(
+                            child: SingleChildScrollView(
+                              child: SelectableText(taskName,
+                              minLines: 1,
+                              maxLines: 5,
+                              style: const TextStyle(
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  letterSpacing: 2),
                         ),
+                            ),
+                          ),
                         ]
                       ),
                     ),
