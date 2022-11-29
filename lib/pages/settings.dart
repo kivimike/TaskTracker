e718e59becca30c6154d8d8b31b7c78c7472e812
Mikhail Kivi <kivimike@gmail.com>
2022-11-29 06:22:37 +0500

    #41

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -7,18 +7,57 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        //centerTitle: true,
-        backgroundColor: Colors.green.shade400,
-        title: Container(
-            child: Text('Settings',
-                style: TextStyle(
-                    letterSpacing: 1.5,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 28))),
-      ),
-      drawer: SideNavigationBar(),
-      backgroundColor: Colors.grey[300],
-    );
+        appBar: AppBar(
+          //centerTitle: true,
+          backgroundColor: Colors.green.shade400,
+          title: Container(
+              child: Text('Settings',
+                  style: TextStyle(
+                      letterSpacing: 1.5,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 28))),
+        ),
+        drawer: SideNavigationBar(),
+        backgroundColor: Colors.grey[300],
+        body: Column(
+          children: [
+            GestureDetector(
+              onTap: (){
+              },
+              child: Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: Row(
+                  children: [
+                    Text(
+                      'Import data',
+                      style: TextStyle(letterSpacing: 1.2, fontSize: 16),
+                    ),
+                    Spacer(),
+                    Icon(Icons.upload,
+                    color: Colors.grey.shade600,),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: (){print('object');},
+              child: Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Row(
+                  children: [
+                    Text(
+                      'Export data',
+                      style: TextStyle(letterSpacing: 1.2, fontSize: 16),
+                    ),
+                    Spacer(),
+                    Icon(Icons.download,
+                        color: Colors.grey.shade600),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
